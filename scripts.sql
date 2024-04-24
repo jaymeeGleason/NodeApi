58f8f0a05aaea58a919716fa2ac2657a8ef43b7a
jaymeeGleason <jamielynngleason0@gmail.com>
2024-04-24 11:51:07 -0400

    Added weather

--- scripts.sql
+++ scripts.sql
@@ -1,28 +1,33 @@
 CREATE SEQUENCE persona_seq START 1;
 CREATE SEQUENCE profile_seq START 1;
-CREATE SEQUENCE profile_persona_seq START 1;
+CREATE SEQUENCE product_seq START 1;
+
+CREATE TABLE profile(
+    id INTEGER NOT NULL DEFAULT nextval('profile_seq'),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_profile PRIMARY KEY(id)
+);
 
 CREATE TABLE persona(
     id INTEGER NOT NULL DEFAULT nextval('persona_seq'),
+    profile_id INTEGER NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     latitude DECIMAL(9,6),
     longitude DECIMAL(9,6),
-    CONSTRAINT pk_persona PRIMARY KEY(id)
-);
-CREATE TABLE profile(
-    id INTEGER NOT NULL DEFAULT nextval('profile_seq'),
     interests VARCHAR(1000),
-    CONSTRAINT pk_profile PRIMARY KEY(id)
+    CONSTRAINT pk_persona PRIMARY KEY(id),
+    CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profile(id),
+
 );
 
-CREATE TABLE profile_persona(
-    id INTEGER NOT NULL DEFAULT nextval('profile_persona_seq'),
+CREATE TABLE product(
+    id INTEGER NOT NULL DEFAULT nextval('product_seq'),
     persona_id INTEGER NOT NULL,
-    profile_id INTEGER NOT NULL,
-    CONSTRAINT pk_profile_persona PRIMARY KEY(id),
+    description VARCHAR(500)
+    CONSTRAINT pk_product PRIMARY KEY(id),
     CONSTRAINT fk_persona FOREIGN KEY (persona_id) REFERENCES persona(id),
-    CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profile(id)
 );
 
 -- DROP TABLE persona;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 app.use(express.json());
 
@@ -8,25 +9,83 @@ const port = process.env.PORT;
 const pool = require('./db');
 
 //routes
-app.get('/personas', async(req, res) => {
+//Profile
+app.post('/profile', async(req, res) => {
+    const {firstName, lastName} = req.body;
     try {
-        const data = await pool.query("SELECT * FROM persona");
-        console.log(data);
-        res.status(200).send(data);
+        await pool.query("INSERT INTO profile (first_name, last_name) VALUES ($1, $2)", [firstName, lastName]);
+        res.status(200).send({message: "Successfully added profile"});
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
-    }});
-
-app.post('/persona', async(req, res) => {
-    const {firstName, lastName, latitude, longitude} = req.body;
+    }
+});
+//Product
+app.post('/product', async(req, res) => {
+    const {personaId, description} = req.body;
     try {
-        await pool.query("INSERT INTO persona (first_name, last_name, latitude, longitude) VALUES ($1, $2, $3, $4)", [firstName, lastName, latitude, longitude]);
+        await pool.query("INSERT INTO product (persona_id, description) VALUES ($1, $2)", [personaId, description]);
+        res.status(200).send({message: "Successfully added product"});
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+//Persona
+app.post('/persona', async(req, res) => {
+    const {profileId, firstName, lastName, latitude, longitude, interests} = req.body;
+    try {
+        await pool.query("INSERT INTO persona (profile_id, first_name, last_name, latitude, longitude, interests) VALUES ($1, $2, $3, $4, $5, $6)", [profileId, firstName, lastName, latitude, longitude, interests]);
         res.status(200).send({message: "Successfully added persona"});
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
 });
+
+app.get('/personas', async(req, res) => {
+
+    try {
+        const data = await pool.query("SELECT * FROM persona");
+        res.status(200).send(data);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }});
+
+app.get('/persona', async(req, res) => {
+        const { id } = req;
+        
+        try {
+            const data = await pool.query("SELECT * FROM persona where id = $1", [id]);
+            res.status(200).send(data);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    }
+);
+
+// Weather
+
+app.get('/weather', async(req, res) => {    
+    try {
+        const url = await getWeather('https://api.weather.gov/points/38.8894,-77.0352');
+        const weather = await getWeather(url.properties.forecast);
+        res.status(200).send(weather.properties.periods);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+);
+
+const options = { method: 'GET', headers: { accept: 'application/json' } };
+const getWeather= async(url) => {
+    const response =  await fetch(url);
+    return await response.json();
+}
+
+
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`));
